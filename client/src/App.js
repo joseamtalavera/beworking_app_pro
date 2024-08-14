09c7f2d5cb1b454d6670c8ac1abdc8aacb39a053
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2024-08-14 18:40:59 +0300

    home

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { Typography} from '@mui/material';
+//import ResponsiveDrawer from './components/Menu/ResponsiveDrawer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <ResponsiveDrawer /> */}
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Typography variant="h4">
+          Welcome to the BeWorking new Website!
+        </Typography>
+      </div>
     </div>
   );
 }
